e094148784a0eddb8aafb1c442dc5c0c92a5951e
GeorgeHester <junk142003@gmail.com>
2019-10-29 17:46:25 +0000

    New system

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 //nodejs imports
 const fs = require('fs');
+var cors = require('cors')
+const express = require('express');
+const app = express();
 const request = require('request-promise');
 //log for app startup
-console.log('[ App: Start ]');
+console.log('[ App: Start ][ ApiSys ]');
 //array of all urls
 const urls = [
     `https://newsapi.org/v2/top-headlines?country=gb&pageSize=3&apiKey=4c75d0c75a2c41f9a1c8563fa358e81b`,
@@ -15,6 +18,42 @@ const files = [
     './json/tech_news_wired.json',
     './json/tech_news_techradar.json',
 ];
+//allow Cross-Origin Resource Sharing
+app.use(cors())
+//api get request function
+app.get('/newstech/wired', (req, res) => {
+    //opens the request file to send back
+    var techrawdata = fs.readFileSync('./json/tech_news_wired.json');
+    var techdata = JSON.parse(techrawdata);
+    //log to show that the file has been read
+    console.log('[ Read: TechWired ]');
+    //response sending
+    res.send(techdata);
+    //log to say the the file has been sent
+    console.log('[ Sent: TechWired ]');
+});
+
+app.get('/newstech/techradar', (req, res) => {
+    var techrawdata = fs.readFileSync('./json/tech_news_techradar.json');
+    var techdata = JSON.parse(techrawdata);
+    console.log('[ Read: TechTechRadar ]');
+    res.send(techdata);
+    console.log('[ Sent: TechTechRadar ]');
+});
+
+app.get('/newsbreak', (req, res) => {
+    var breakrawdata = fs.readFileSync('./json/break_news.json');
+    var breakdata = JSON.parse(breakrawdata);
+    console.log('[ Read: NewsBreak ]');
+    res.send(breakdata);
+    console.log('[ Sent: NewsBreak ]');
+});
+
+app.listen(process.env.PORT || 54321, function () {
+    console.log("[ Port: %d ][ Mode: %s ]", this.address().port, app.settings.env);
+});
+//log for app startup
+console.log('[ App: Start ][ FileSys ]');
 //function to make a request and write the data to file
 async function getdata() {
     //makes a request to the news api
@@ -38,4 +77,4 @@ async function getdata() {
     };
 };
 //timer for api gets
-setInterval(getdata, 600000);
+setInterval(getdata, 900000);
